06a9846fbdf8cc685e23bbfc0c3fc5babe58c749
TX-9 <jacobleecs@gmail.com>
2019-05-06 17:32:42 -0400

    add chalk

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const name = require('./utils.js');
 
 console.log(name);
 
 console.log(validator.isEmail('jacob@aa.com'));
+
+const greenMsg = chalk.green.bold('Success');
+console.log(greenMsg);
